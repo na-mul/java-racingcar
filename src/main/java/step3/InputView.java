91d9aed31754298ea6195b0829c28978506b9631
Donghwan Noh <namul.bap@gmail.com>
2020-12-07 00:58:22 +0900

    feat(view): 플레이어로부터 자동차의 이동을 시도할 횟수를 입력받는다.
    InputView 클래스에 메소드 추가
    - inputMoveTryMaxCount : 사용자에게 이동시도횟수를 입력받는 메소드. 올바른 입력값이 들어올 때까지 반복됨.
    
    BREAKING CHANGE:
    - playground :
      - play 메소드에 inputMoveTryMaxCount를 통한 입력을 받는 부분을 추가함.

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -12,8 +12,17 @@ public class InputView {
 	public static int inputNumberOfCars() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 		String input = scanner.nextLine();
-		if (!Pattern.matches("^[1-9]+$", input)) {
+		if (!Pattern.matches("^[0-9]+$", input)) {
 			return inputNumberOfCars();
+		}
+		return Integer.parseInt(input);
+	}
+
+	public static int inputMoveTryMaxCount() {
+		System.out.println("시도할 회수는 몇 회 인가요?");
+		String input = scanner.nextLine();
+		if (!Pattern.matches("^[0-9]+$", input)) {
+			return inputMoveTryMaxCount();
 		}
 		return Integer.parseInt(input);
 	}
